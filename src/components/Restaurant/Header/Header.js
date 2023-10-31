8489232584d2eee83b46b2aac3fccff5be667a0f
toba456 <tobias.marroquin@gmail.com>
2023-10-31 20:46:14 -0300

    fix-bug

--- src/components/Restaurant/Header/Header.js
+++ src/components/Restaurant/Header/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View } from "react-native";
-import { Text, Rating, AirbnbRating } from "@rneui/base";
+import { Text, AirbnbRating } from "@rneui/base";
 import { styles } from "./Header.styles";
 
 export const Header = ({ restaurant }) => {
